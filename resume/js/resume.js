41add77a1d5104f1330e2686962e7c2d2c4d4ae8
CJ0423 <b09785157888@gmail.com>
2022-12-22 01:16:05 +0800

    l

--- resume/js/resume.js
+++ resume/js/resume.js
@@ -1,12 +1,17 @@
 const appear =document.querySelector(".nav-left")
-
+const body =document.querySelector("body")
+console.log(body.offsetHeight)
 document.addEventListener('scroll',function(){
     let currentPos =  window.scrollY;
+    let lastPos =  window.scrollY;
     //   往下滑
-    if(currentPos > lastPos){
-        appear.style.top = "-60px"; //讓nav bar消失
+    console.log(appear.style.top)
+    
+    if(currentPos <(body.offsetHeight/4) ){
+        appear.style.display = "none"; //讓nav bar消失
+        
     }else{
-        appear.style.top = "0px"; //讓nav bar出現
+        appear.style.display = ""; //讓nav bar出現
     }
     lastPos = currentPos;//再記住現在位置，跟未來的位置做比較
   });
