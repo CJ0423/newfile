d056d2a4501ece1eea421fae7fe1ba4da5664a9f
CJ0423 <b09785157888@gmail.com>
2023-04-27 11:27:27 +0800

    ˇ

--- rereact/src/App.js
+++ rereact/src/App.js
@@ -1,11 +1,27 @@
-import './App.css';
+const names=[{id:"1",name:"胖胖"},{id:2,name:"牛奶"}]
+const text=false
+const type=()=>{return "hi"}
+const fat=false
+function hi(){
+  console.log("有")
+  return "嗨"
+}
+
 
 function App() {
   return (
     <div className="App">
-    app
+     {names.map((name)=><span key={name.id}>{name.name}</span>)}
+     {text  ? <div>胖胖</div>:null }
+     {text &&   <div>胖胖牛</div> }
+     {true && <span>下一個</span>}
+    {type(1)}
+    {hi()}
+    {fat ? <div>胖胖</div>:<div>瘦弱的胖</div>}
+
     </div>
+
   );
 }
-
+//  陣列.map((每一個組件)＝>要幹嘛)
 export default App;
